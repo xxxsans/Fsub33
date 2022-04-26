93eda3eb2ba82563fd7ef71ed6672080f56a209f
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-04-26 17:34:35 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,6 +66,26 @@ class Bot(Client):
                 )
                 sys.exit()
 
+if FORCE_SUB_CHANNEL1:
+            try:
+                link = (await self.get_chat(FORCE_SUB_CHANNEL1)).invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL1)
+                    link = (await self.get_chat(FORCE_SUB_CHANNEL1)).invite_link
+                self.invitelink = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning(
+                    "Bot tidak dapat Mengambil link invite dari FORCE_SUB_CHANNEL1!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
+                )
+                self.LOGGER(__name__).info(
+                    "Bot Berhenti. Gabung Group https://t.me/obrolansuar untuk Bantuan"
+                )
+                sys.exit()
+
         if FORCE_SUB_GROUP:
             try:
                 link = (await self.get_chat(FORCE_SUB_GROUP)).invite_link
