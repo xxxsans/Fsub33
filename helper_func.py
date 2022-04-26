fe27139baf262dc16d512ba787bb4358c0aa0951
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-04-26 18:03:12 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -10,7 +10,7 @@ from pyrogram import filters
 from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
-from config import ADMINS, FORCE_SUB_CHANNEL, FORCE_SUB_GROUP
+from config import ADMINS, FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL1, FORCE_SUB_GROUP
 
 
 async def subschannel(filter, client, update):
@@ -22,6 +22,20 @@ async def subschannel(filter, client, update):
     try:
         member = await client.get_chat_member(
             chat_id=FORCE_SUB_CHANNEL, user_id=user_id
+        )
+    except UserNotParticipant:
+        return False
+
+    return member.status in ["creator", "administrator", "member"]
+ async def subschannel(filter, client, update):
+    if not FORCE_SUB_CHANNEL1:
+        return True
+    user_id = update.from_user.id
+    if user_id in ADMINS:
+        return True
+    try:
+        member = await client.get_chat_member(
+            chat_id=FORCE_SUB_CHANNEL1, user_id=user_id
         )
     except UserNotParticipant:
         return False
