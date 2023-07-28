766f1e336c29ab146a19f337f19975079673166a
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2023-07-28 11:09:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,6 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
-FORCE_SUB_CHANNEL1 = int(os.environ.get("FORCE_SUB_CHANNEL1", "0"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
