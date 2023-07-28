650343650c0c91ed81c66fc9f20ba9184abc5bb0
zergiogithub <138329164+zergiorvdr@users.noreply.github.com>
2023-07-28 12:02:07 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -10,7 +10,7 @@ from pyrogram import filters
 from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
-from config import ADMINS, FORCE_SUB_CHANNEL, FORCE_SUB_GROUP, FORCE_SUB_CHANNEL2
+from config import ADMINS, FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL1, FORCE_SUB_GROUP
 
 
 async def subschannel(filter, client, update):
@@ -29,15 +29,15 @@ async def subschannel(filter, client, update):
     return member.status in ["creator", "administrator", "member"]
 
 
-async def subschannel2(filter, client, update):
-    if not FORCE_SUB_CHANNEL2:
+async def subschannel1(filter, client, update):
+    if not FORCE_SUB_CHANNEL1:
         return True
     user_id = update.from_user.id
     if user_id in ADMINS:
         return True
     try:
         member = await client.get_chat_member(
-            chat_id=FORCE_SUB_CHANNEL2, user_id=user_id
+            chat_id=FORCE_SUB_CHANNEL1, user_id=user_id
         )
     except UserNotParticipant:
         return False
@@ -62,7 +62,7 @@ async def subsgroup(filter, client, update):
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL:
         return True
-    if not FORCE_SUB_CHANNEL2:
+    if not FORCE_SUB_CHANNEL1:
         return True
     if not FORCE_SUB_GROUP:
         return True
@@ -81,7 +81,7 @@ async def is_subscribed(filter, client, update):
         return False
     try:
         member = await client.get_chat_member(
-            chat_id=FORCE_SUB_CHANNEL2, user_id=user_id
+            chat_id=FORCE_SUB_CHANNEL1, user_id=user_id
         )
     except UserNotParticipant:
         return False
@@ -148,5 +148,5 @@ async def get_message_id(client, message):
 
 subsgc = filters.create(subsgroup)
 subsch = filters.create(subschannel)
-subsch2 = filters.create(subschannel2)
+subsch1 = filters.create(subschannel1)
 subsall = filters.create(is_subscribed)
